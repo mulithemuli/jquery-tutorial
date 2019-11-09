db25d95db7bbad3353e1481a07e120d5f8ae0a7c
mulithemuli <philipp.lohmeyer@gmail.com>
2019-11-09 14:21:30 +0100

    adding chapter 1 / lesson 4 in separate directories

---
+++ Chapter_1/Lesson_4/README.md
@@ -0,0 +1,9 @@
+# Lesson 4 – removing the buttons
+
+The buttons are useful but not necessarily relevant to every user. We would like to hide (but not remove) the buttons and delegate the change of an input to the click of the button.
+
+Covered in this lesson
+- event delegation
+- jQuery DOM manipulation
+
+When JavaScript is active we would like to remove the button from the view but trigger its click action when the input has been changed.
---
+++ Chapter_1/Lesson_4/Solution/script.js
@@ -0,0 +1,18 @@
+(function($) {
+    let clickCounter = 0;
+
+    $('input[type=text]').on('change', e => {
+        $('button', $(e.currentTarget).closest('.row')).trigger('click');
+    });
+
+    $('button').hide().on('click', e => {
+        let container = $(e.currentTarget).closest('.row');
+        let text = $('.typed-text', container).text();
+        let typedText = $('input', container).val();
+        if (text !== typedText) {
+            clickCounter += 1;
+            $('.times-clicked').text(clickCounter);
+            $('.typed-text', container).text(typedText);
+        }
+    });
+}(jQuery));
